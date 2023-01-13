bb5924a962845ab64654e1bf1eb44cad8527a2be
unknown <capar.2022113tads0049@aluno.ifpi.edu.br>
2023-01-12 22:43:13 -0300

    Inserção de dados ao banco de dados pelo cadastro controller

--- src/main/java/com/controllers/CadastroController.java
+++ src/main/java/com/controllers/CadastroController.java
@@ -2,6 +2,10 @@ package com.controllers;
 
 import java.io.IOException;
 
+
+import com.classes.Professor;
+import com.utils.ProfessorDAO;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -24,11 +28,20 @@ public class CadastroController {
     @FXML private TextField txTitulacao;
 
     @FXML void Cadastrar() {
-        txCpf.getText();
-        txEspecializacao.getText();
-        txNome.getText();
-        Double.parseDouble(txSalario.getText());
-        txTitulacao.getText();
+        Professor professorAt = new Professor();
+        professorAt.setCpf(txCpf.getText());
+        professorAt.setEspecializacao(txEspecializacao.getText());
+        professorAt.setNome(txNome.getText());
+        professorAt.setSalario(Double.parseDouble(txSalario.getText()));
+        professorAt.setTitulacao(txTitulacao.getText());
+
+        txCpf.setText("");
+        txEspecializacao.setText("");
+        txNome.setText("");
+        txTitulacao.setText("");
+
+        ProfessorDAO professor  = new ProfessorDAO();
+        professor.insert(professorAt);
     }
 
     @FXML
